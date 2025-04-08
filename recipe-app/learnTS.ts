9fea0f727d336d12968713ba1924a17ee9d0992c
DianaGaspar <diana.gaspar09@yahoo.com>
2025-04-08 13:05:09 +0300

    Add recipes

---
+++ recipe-app/learnTS.ts
@@ -0,0 +1,12 @@
+let hi: string = 'Hello';
+console.log(hi);
+
+enum StatusCodes {
+    NotFound = 404,
+    Succes = 200,
+    BadRequest = 400,
+}
+let nf: StatusCodes = StatusCodes.NotFound;
+
+const recipeNames: string[] = [];
+
